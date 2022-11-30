e43de1d8c3a1b54cd73e9019eea8b87bc19468ce
jmarsil <joseph.marsilla@mail.utoronto.ca>
2022-11-30 16:56:46 -0500

    update

--- slurm/wol-oar-train-h4h1.sh
+++ slurm/wol-oar-train-h4h1.sh
@@ -61,7 +61,7 @@ data_path= "/cluster/projects/radiomics/Temp/joe/RADCURE_VECTOR_UPDATE/" # "/sto
 home_path= "/cluster/home/jmarsill/ptl-oar-segmentation/" # "/h/jmarsilla/ptl-oar-segmentation/" # server "/home/gpudual"
 model_path="/cluster/projects/radiomics/Temp/joe/models-1222" # "/h/jmarsilla/models" #"--model-path"
 use_16bit=False # save model to...
-path=$home_path'train.py' #train_2 if training new model
+path="/cluster/home/jmarsill/ptl-oar-segmentation/train.py" #train_2 if training new model
 print_outputs_to=$model'_'$tag'_'$(date "+%b_%d_%Y_%T").txt
 
 echo 'Started python script.'
