efb009510f1f5913cc888c1eb881e59e3ad68a45
jmarsil <joseph.marsilla@mail.utoronto.ca>
2023-01-19 20:53:50 -0500

    update exclusions..

--- utils/modules_vector.py
+++ utils/modules_vector.py
@@ -70,7 +70,8 @@ class SegmentationModule(pl.LightningModule):
         # fall within the following ranges... -390 < meanHU < -420; 205 < stdHU < 245
 
         path_ = self.hparams.root + f"/config_{self.tag}.json"
-        exclude_ = ["RADCURE-0543", "RADCURE-1856", "RADCURE-2503", "RADCURE-0079"]
+        exclude_ = ["RADCURE-0543", "RADCURE-1856", "RADCURE-2503", "RADCURE-0079",
+                    "RADCURE-2205"]
         try:
             # if os.path.isfile(self.hparams.is_config) is True:
             # ideally this should be a .json file in the format of self.data_config
