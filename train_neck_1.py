60865f5705d655f382a667729f0b6265235d66b3
jmarsill <joseph.marsilla@mail.utoronto.ca>
2023-04-27 19:24:36 -0400

    update train for necks

--- train_neck_1.py
+++ train_neck_1.py
@@ -49,8 +49,8 @@ def main(args):
             strategy='ddp', # should be same as args.backend..., # stochastic_weight_avg=True, # pass to callbacks if required...
             reload_dataloaders_every_n_epochs=1,
             # limit_train_batches=0.2,
-            limit_train_batches=0.7,
-            limit_val_batches=0.4,
+            limit_train_batches=0.9,
+            limit_val_batches=0.5,
             default_root_dir=model.hparams.root,
             max_epochs=model.hparams.n_epochs,
             # log_gpu_memory='min_max',
